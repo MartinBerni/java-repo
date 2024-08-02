316ec7e9271b607a6cf3f11ba8007583ad342463
Martin Berni <martinberni13@gmail.com>
2024-08-01 21:25:23 -0300

    aprendiendo todo sobre entradas de datos

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -1,47 +1,43 @@
 package holamundo;
+
+import java.util.Scanner; //IMPORTACION AL TOCAR EL ERROR DEL SCANNER
+
 public class HolaMundo {
 
     public static void main(String[] args) {
+      //PARA LA ENTRADA DE DATOS Y ALMACENAMIENTO EN VAIRABLES DEBEMOS PONER:
+      Scanner entrada = new Scanner (System.in); //la variable entrada es el medio por el cual meto los datos
+      //AL PONERLO SOLO, SALTARA ERROR LO CUAL REQUEIRE QUE IMPORTEMOS EL JAVA.UTIL (TOCANDO EL ERROR SE COLOCA SOLO)
+      
+      float numero;
+      String nombre; 
+      char letra;
+      
+      System.out.print("Escribe un nuemro: "); 
+      numero = entrada.nextFloat(); //AL PONER EL ENTRADA NESESITA SABER EL TIPO DE ENTRADA, POR ESO SE COLOCA EL NEXTINT
+      
+      //SI NECESITO MOSTRAR UN int COLOCO .NEXTINT
+      //EN LOS FLOAT DEBO USAR SI O SI LA COMA, NO EL PUNTO
+      
+        System.out.println("El nuemero jaun es: "+numero);
+        
+      //PROCEDIMIENTO PARA CADENA
+        System.out.print("Ingrese cadena: ");
+        nombre = entrada.next(); // EL .NEXT SOLO GUARDA LA PRIMERA PALABRA
+        System.out.println(nombre);
+        
+        System.out.print("ingrese la cadena nuevamente: ");
+        nombre = entrada.nextLine(); // EL .NEXTLINE GUARDA TODA LA CADENA
+        System.out.println(nombre);
+        
+      //PROCEDIMINETO PARA UN CHAR
+      
+        System.out.println("ingrese la letra que quiera");
+        letra = entrada.next().charAt(0); //CON EL CHARAT LOGRAMOS QUE SE MUESTRE LA LETRA EN LA POSICION 0 ES DECIR LA QUE YO COLOQUE
+        System.out.println("La letra es: "+letra);
         
         
-        //VARIABLES PRIMITIVAS
+      
         
-        // VARIABLES ENTERAS
-        
-       byte corto=12; //va desde -128 a 127 
-       short longer=10000; //desde -32.768 hasta 32.768
-       int usual=50000; //desde -2 millones a 2 millones (mas usado) 
-       long longest=100000; //mas largo de todos}
-       
-       //PARA IMPRIMIR UNA VARIABLE PONEMOS +"NOMBRE DE LA VARIABLE"
-        System.out.println("el nuemero es: "+corto);
-        
-       //VARIABLES CON NUMEROS REALES
-       
-       float real=3.45f; //Variable real DEBO COLOCAR F DESPUES DEL NUMERO CON COMA
-       double doble=1000.15; //variable real pero muy grande (NO HACE FALTA LA F)
-       
-       //VARIABLES TIPO CARACTER
-       
-       char inicial='a'; //guarda una letra, numero, simbolo y va con commillas simples
-       boolean estado=true; //toma dos valores unicamente V o F
-       
-       
-       //-----VARIABLES NO PRIMITIVAS-----ALMACENAN NULL Y PERMITEN LOS METODOS (NOMBRE DE LA VARIABLE Y UN .)
-       
-       Integer hola=null; //ME DEJA GUARDAR TANTO ENTEROS COMO EL NULL (VACIO), YA QUE INT NO ME DEJARIA
-       
-       String palabras = "Esta es la variable palabra"; //CADENA DE CARACTERES
-       
-       //CONSTANTES EN JAVA
-       
-       final int esto_es_cte = 3; //CON EL FINAL DEFINIMOS UNA CTE 
-       
-       
-       
-       
-       
-       
-    
     }
 }
