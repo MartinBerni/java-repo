dcabeed6eb802b91018cef550ebd4449861ad6bc
Martin Berni <martinberni13@gmail.com>
2024-08-04 15:15:03 -0300

    Aprendiendo math

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -2,44 +2,24 @@
 
 package holamundo;
 
-import java.util.Scanner;
-
 public class HolaMundo {
 
     public static void main(String[] args) {        
-        Scanner input = new Scanner(System.in);
-        
-        float num1,num2,suma,resta,div,mult,resto;
-        
-        System.out.print("Dame el numero 1: ");
-        num1 = input.nextFloat();
-        System.out.print("Dame el numero 2: ");
-        num2 = input.nextFloat();
-        
-        suma = num1 + num2;
-        resta = num1 - num2;
-        mult = num1 * num2;
-        div = num1/num2;
-        resto = num1%num2;
-        
-        int numero = 3,y;
-        
-        numero += 5; //numero = numero + 5 es lo mismo
-        numero -= 5; //numero = numero - 5 es lo mismo
-        numero /= 5; //numero = numero / 5 es lo mismo
-        numero *= 5; //numero = numero * 5 es lo mismo
-        numero %= 5; //numero = numero % 5 es lo mismo
-        
-        numero++; //Inc
-        numero--; //Dec
         
         
-        numero=2;
-        y = numero++ ; // y valdra 2 y numero despues se incrementara y valdra 3
-        //Si usamos el operador como prefijo (++numero) primero se incrementa y luego se asigna, entonces y valdria 3
+        //RAIZ
+        double raiz = Math.sqrt(9); //Math.sqrt es la raiz cuadrada simpre con double
+       
+        //EXPONENTE
+       double base = 2 , exponente = 3; //Para el exponente uso todo double
+       double resultado = Math.pow(base, exponente); //Math.pow es el exponente (base, exponenete)
         
-        /*numero=2;
-        y = numero-- mismo caso para esto */
-        
+       //REDONDEO
+       double numero = 3.4;
+       long redondeo = Math.round(numero); //Cuando pongo Math.round el resultado lo almaceno en un long o int
+       
+       //NUMERO RANDOM
+       double raro = Math.random(); //Genera un numero ramdom
+       
     }
 }
