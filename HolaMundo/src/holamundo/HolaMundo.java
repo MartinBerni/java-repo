e8259b44056d93006dc97ad954e3bfaea9889cbe
Martin Berni <martinberni13@gmail.com>
2024-08-04 19:34:59 -0300

    Aprendiendo if y switch

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -2,24 +2,46 @@
 
 package holamundo;
 
+import javax.swing.JOptionPane;
+
 public class HolaMundo {
 
     public static void main(String[] args) {        
         
+        int numero, dato = 10;
         
-        //RAIZ
-        double raiz = Math.sqrt(9); //Math.sqrt es la raiz cuadrada simpre con double
-       
-        //EXPONENTE
-       double base = 2 , exponente = 3; //Para el exponente uso todo double
-       double resultado = Math.pow(base, exponente); //Math.pow es el exponente (base, exponenete)
         
-       //REDONDEO
-       double numero = 3.4;
-       long redondeo = Math.round(numero); //Cuando pongo Math.round el resultado lo almaceno en un long o int
-       
-       //NUMERO RANDOM
-       double raro = Math.random(); //Genera un numero ramdom
-       
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese hola"));
+        
+        if (numero == dato) {
+            JOptionPane.showMessageDialog(null, "EL numero es 10");
+        }else{
+            JOptionPane.showMessageDialog(null, "El numero no es 10");
+        }
+        
+        // numero == dato igual
+        // numero != dato distinto
+        // numero > dato mayor
+        // numero < dato menor
+        // numero <= dato menor igual
+        // numero >= dato mayor igual
+        
+        
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Dame el numero para ver el switch del 1 al 5: "));
+        
+        switch (num) {
+            case 1: JOptionPane.showMessageDialog(null, "El numero es 1");
+                    break;                                                 
+            case 2: JOptionPane.showMessageDialog(null, "El numero es 2");
+                    break;
+            case 3: JOptionPane.showMessageDialog(null, "El numero es 3");
+                    break;
+            case 4: JOptionPane.showMessageDialog(null, "El numero es 4");
+                    break;
+            case 5: JOptionPane.showMessageDialog(null, "El numero es 5");
+                    break;
+            default: JOptionPane.showMessageDialog(null, "EL numero no esta entre 1-5");
+        }
+        //breack termina el segun caso y el default es el caso q no se cumplio nunca 
     }
 }
