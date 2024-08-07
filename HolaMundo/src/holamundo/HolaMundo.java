23bf7c709e6aaef830ce65277d47c79ed5317057
Martin Berni <martinberni13@gmail.com>
2024-08-07 00:36:47 -0300

    Aprendiendo los iteradores Do while, while, for

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -2,46 +2,45 @@
 
 package holamundo;
 
-import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 
 public class HolaMundo {
 
     public static void main(String[] args) {        
+      Scanner entrada = new Scanner(System.in);
+    
+      //WHILE
+      
+      System.out.println("WHILE");
+      int i=1, cont;
         
-        int numero, dato = 10;
+        System.out.print("Diga cuantos numeros quiere imprimir: ");
+        cont = entrada.nextInt();
         
-        
-        numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese hola"));
-        
-        if (numero == dato) {
-            JOptionPane.showMessageDialog(null, "EL numero es 10");
-        }else{
-            JOptionPane.showMessageDialog(null, "El numero no es 10");
+        while (i<=cont){
+            System.out.println(i);
+            i++;
         }
         
-        // numero == dato igual
-        // numero != dato distinto
-        // numero > dato mayor
-        // numero < dato menor
-        // numero <= dato menor igual
-        // numero >= dato mayor igual
         
+        //DO while
         
-        int num = Integer.parseInt(JOptionPane.showInputDialog("Dame el numero para ver el switch del 1 al 5: "));
+        System.out.println("DO WHILE");
         
-        switch (num) {
-            case 1: JOptionPane.showMessageDialog(null, "El numero es 1");
-                    break;                                                 
-            case 2: JOptionPane.showMessageDialog(null, "El numero es 2");
-                    break;
-            case 3: JOptionPane.showMessageDialog(null, "El numero es 3");
-                    break;
-            case 4: JOptionPane.showMessageDialog(null, "El numero es 4");
-                    break;
-            case 5: JOptionPane.showMessageDialog(null, "El numero es 5");
-                    break;
-            default: JOptionPane.showMessageDialog(null, "EL numero no esta entre 1-5");
+        int x=1;
+        
+        do {
+            System.out.println(x);
+            x++;
+        }while (x<=5);
+    
+        //FOR
+        
+        int f; 
+        
+        for (f=1; f<=10;f++){
+            System.out.println(f);
         }
-        //breack termina el segun caso y el default es el caso q no se cumplio nunca 
     }
 }
