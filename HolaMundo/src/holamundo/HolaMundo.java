a5f3f13ef4a2c3c8152ca64973238ab1c07fe343
Martin Berni <martinberni13@gmail.com>
2024-08-11 23:24:51 -0300

    Aprendiendo arreglos y for each

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -2,45 +2,51 @@
 
 package holamundo;
 
+
 import java.util.Scanner;
 
 
-public class HolaMundo {
 
-    public static void main(String[] args) {        
-      Scanner entrada = new Scanner(System.in);
+public class HolaMundo {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
     
-      //WHILE
-      
-      System.out.println("WHILE");
-      int i=1, cont;
         
-        System.out.print("Diga cuantos numeros quiere imprimir: ");
-        cont = entrada.nextInt();
         
-        while (i<=cont){
-            System.out.println(i);
-            i++;
+       int [] edad = new int [4];
+       long [] dni = new long[4];
+       float [] estatura = new float[3];
+       double [] estarura = new double[5];
+       boolean [] estado = new boolean[4];
+       char [] sexo = new char [2];
+       String [] nombre = new String [2];
+       
+       //DAR VALORES
+       
+       edad[0]=15;
+       long [] calle = {2,3,8}; //DECLARAMOS E INICIALIZAMOS 
+       
+       //PARA RECORRER USAMOS FOR pero con int i = 0 ; i<6 ; i++ no ponemos ((<=))
+       
+       //PARA LLENAR UN ARREGLO SOLICITAMOS LA CANTIDAD DESEADA POR EL USUARIO DE ESPACIO
+       
+        
+       String [] nombres = {"Juan", "pedro", "Yo", "luz"};
+       
+        for (int i=0; i<nombres.length; i++) { //al poner (nombres.length) java identificara la longitud del arreglo
+            System.out.println(nombres[i]);
         }
+       
+        //for each
         
+        /* for ("TIPO DE DATO DEL ARREGLO" "INDICE" : "NOMBRE DEL ARREGLO"){
+            lo que quiero hacer con el subindice 
+        }
+        */
         
-        //DO while
-        
-        System.out.println("DO WHILE");
-        
-        int x=1;
-        
-        do {
-            System.out.println(x);
-            x++;
-        }while (x<=5);
-    
-        //FOR
-        
-        int f; 
-        
-        for (f=1; f<=10;f++){
-            System.out.println(f);
+        for (String i:nombres)  {
+            System.out.println(i);
         }
     }
 }
