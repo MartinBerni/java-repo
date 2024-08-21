9ad37c69f1b29003c390a456726453f4d0aad896
Martin Berni <martinberni13@gmail.com>
2024-08-21 13:01:16 -0300

    Aprendiendo ordenamiento por insercion

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -14,12 +14,12 @@ public class HolaMundo {
         
         Scanner input = new Scanner(System.in);
         
-        // METODO BURBUJA EN ARREGLO 
-        // ORDENAMOS UNA TRAS OTRO EL QUE ESTAMOS VIENDO CON EL SIGUIENTE
+        // METODO INSERCION
+        // AL ENCONTRAR UN MENOR VA COMPARANDO CON LOS ANTERIORES SI ES MENOR Y LOS VA MOVIENDO
         // EJEMPLO
         
         int [] arreglo;
-        int narreglo, aux;
+        int narreglo, aux, pos;
         
         narreglo = Integer.parseInt(JOptionPane.showInputDialog("Dame el tamaño del arreglo: "));
         
@@ -30,27 +30,28 @@ public class HolaMundo {
             arreglo [i] = input.nextInt();
         }
         
-        //METODO DE BURBUJA
+        //METODO DE INSERCION
         
-        for (int i=0; i<(narreglo - 1); i++) { //COLOCAMOS EL -1 PORQUE CON UNA VUELTA MENOS ESTARA BIEN ORDENADO
-            for (int j=0; j<(narreglo - 1); j++) {//ACA ORDENAMOS
-                if (arreglo [j]>arreglo[j+1]){//SI NUMERO EN EL QUE ESTAMOS ES MAYOR HAY QUE CAMBIARLO
-                    aux = arreglo [j];
-                    arreglo [j] = arreglo [j+1];
-                    arreglo [j+1] = aux;
-                }
+        for (int i=0; i<(narreglo - 1); i++) { //POS INDICA LA POSICION
+            pos = i;
+            aux = arreglo [i];
+            
+            while ((pos>0) && (arreglo[i-1] > aux)){ //POS > 0 PORQUE NO COMPARAMOS EL PRIMERO
+                arreglo [pos] = arreglo [pos-1];
+                pos--;
             }
+            arreglo [pos] = aux;
         }
         
         //MOSTRANDO EL ARREGLO
         System.out.println("Arreglo creciente: ");
         for (int i:arreglo) {
-            System.out.print(i+" - ");
+            System.out.print(i+" / ");
         }
         System.out.println("");
         System.out.println("Arreglo decreciente: ");
         for (int i=(narreglo - 1); i>=0; i--) {
-            System.out.print(arreglo[i]+" - ");
+            System.out.print(arreglo[i]+" / ");
         }
         
     }
